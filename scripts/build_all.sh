315236c3577f77da420238f39b5cfda0ae167225
botanicvelious <cyberempires@gmail.com>
2024-03-30 22:28:52 -0600

    bump ffmpeg to 7 and enable arm64 osx  (#13)
    * Update ffmpeg
    
    * Update build_all.sh
    
    * Update build_all.sh
    
    * Update build_all.sh
    
    * Update all.yml
    
    * Update all.yml
    
    * Update all.yml
    
    * Update all.yml
    
    * Update ffmpeg
    
    * Update all.yml

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -260,10 +260,6 @@ function ffmpeg_install() {
         "--enable-avutil" \
         "--enable-swresample" \
         "--enable-swscale" \
-        "--enable-dct" \
-        "--enable-mdct" \
-        "--enable-rdft" \
-        "--enable-fft" \
         "--disable-devices" \
         "--disable-encoders" \
         "--disable-filters" \
